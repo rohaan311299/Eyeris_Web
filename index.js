5786495ccf872303c67c4bd39632264169b62244
Bhavyalakhani <bhavyalakhani89@gmail.com>
2021-05-09 10:56:03 +0530

    ADDED: routes files

--- index.js
+++ index.js
@@ -21,10 +21,36 @@ const server = app.listen(process.env.PORT, () => {
     console.log('App listening on port 5000!'.blue.bold);
 });
 
-// Hnadle unhandled promise rejection
-process.on('unhandledRejection', (err,promise) => {
-    console.log(err.message)
-    server.close(() => process.exit(1))
-})
+/**
+ * Error handler.
+ * Sends 400 for Mongoose validation errors.
+ * 500 otherwise.
+ * Do all error handling here.
+ */
+ app.use((err, req, res, next) => {
+    console.log("Async error handler")
+    console.log(err);
+  
+    if (err.name === 'ValidationError') {
+      return res.status(400).json(err.errors);
+    }
+    
+    if (err.name === 'MongoError') {
+        console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+        return res.status(400).json({err:err.keyPattern});
+      }
+    
+    
+    return res.status(500).json(err);
+  });
+  
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log("UNHANDLE")
+    console.log(`Error: ${err.message}`.red.bold.underline);
+    //close server and exit process
+    server.close(() => process.exit(1));
+});
 
 
