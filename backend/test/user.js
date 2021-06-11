d2633891022ab80e47255cec50a1675037f2cbc3
Bhavyalakhani <bhavyalakhani89@gmail.com>
2021-06-11 20:55:41 +0530

    MODF: test/user.js

--- backend/test/user.js
+++ backend/test/user.js
@@ -21,8 +21,28 @@ describe("Users API",() => {
             .post("/api/v1/user/login")
             .send((details))
             .end((err,response) => {
-                response.should.have.status(200);
+                response.should.have.a.status(200);
                 response.body.should.have.property("token");
+                response.body.should.have.property("sendUser").property("cart");
+
+                done();
+            })
+        })
+    })
+
+        describe("GET /api/v1/user/me",() => {
+        it("GET user through token",(done) => {
+            const header = {
+                "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwOThkZjQ4NjM5Y2U1MWQ0MDljNWVjYiIsImlhdCI6MTYyMzQyMDg5MCwiZXhwIjoxNjI2MDEyODkwfQ.2a0atDMNqZjGtg7Pxjp1Y3rv7yGPoCUVBSYi69DjQLQ"
+            }
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwOThkZjQ4NjM5Y2U1MWQ0MDljNWVjYiIsImlhdCI6MTYyMzQyMDg5MCwiZXhwIjoxNjI2MDEyODkwfQ.2a0atDMNqZjGtg7Pxjp1Y3rv7yGPoCUVBSYi69DjQLQ"
+
+            chai.request(server)
+            .get("/api/v1/user/me")
+            .set('Authorization','Bearer ' + token)
+            .end((err,response) => {
+                response.should.have.a.status(200);
+
                 done();
             })
         })
